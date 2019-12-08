9f4c0c0679b406097fe7bc226f44f91a2852e226
Ubuntu <mgd67@cornell.edu>
2019-12-08 15:34:24 +0000

    adding line breaks

--- save_results.sh
+++ save_results.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Save results from training on aws s3 bucket
 
+echo ""
 echo "Saving Model Weights"
 aws s3 sync models/ s3://rubix-cube-results/models
+echo ""
 echo "Saving Training Logs"
 aws s3 sync logs/gradient_tape s3://rubix-cube-results/logs/gradient_tape
+echo ""
 echo "Saving Training Configs"
-aws s3 sync logs/config s3://rubix-cube-results/logs/config
+aws s3 sync logs/configs s3://rubix-cube-results/logs/configs
 
 
