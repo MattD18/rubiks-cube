09330dfbdf638b12c7a607329f8cc14d850d84cb
Matthew Dalton <mgd67@cornell.edu>
2020-01-11 16:51:26 -0500

    write-up

--- read_results.sh
+++ read_results.sh
@@ -5,7 +5,7 @@ echo "Reading Model Weights"
 aws s3 cp s3://rubix-cube-results/models/ ./models/ --recursive
 echo ""
 echo "Reading Training Logs"
-aws s3 cp s3://rubix-cube-results/logs/gradeint_tape ./logs/ec2 --recursive
+aws s3 cp s3://rubix-cube-results/logs/gradient_tape ./logs/ec2 --recursive
 echo ""
 echo "Reading Training Configs"
 aws s3 cp s3://rubix-cube-results/logs/configs ./logs/configs --recursive
